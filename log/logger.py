6a7896acfe532d9f933846d3c4aa11f3a8a3412d
Alexey Kurochkin <KurochkinAlexei@yandex.ru>
2025-02-22 19:06:05 +0100

    refactoring

--- log/logger.py
+++ log/logger.py
@@ -1,15 +1,6 @@
 import logging
 
-# logger = logging.getLogger(__name__)
 
-# stream_handler = logging.StreamHandler()
-# file_handler = logging.FileHandler('game.log', encoding='utf-8')
-#
-# logger.addHandler(stream_handler)
-# logger.addHandler(file_handler)
-
-# Конфигурация логгера
-# todo cleanup logger
 class ColoredFormatter(logging.Formatter):
     COLORS = {'DEBUG': '\033[94m',
               'INFO': '\033[92m',
@@ -22,12 +13,8 @@ class ColoredFormatter(logging.Formatter):
         formatter = logging.Formatter(log_fmt)
         return formatter.format(record)
 
-# logging.getLogger().handlers[0].setFormatter(ColoredFormatter())
-logging.basicConfig(level=logging.DEBUG,
-                    # filename='app.log',
-                    # encoding='utf-8',
-                    format='%(asctime)s - %(levelname)s - %(message)s')
-                    # format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 logger = logging.getLogger(__name__)
--- settings.py
+++ settings.py
@@ -5,5 +5,5 @@ SCREENSHOTS_DIR = os.path.join(WORK_DIR, "images", "screenshots")
 TARGETS_DIR = os.path.join(WORK_DIR, "images", "targets")
 
 # logger
-save_log_in_file = True
-show_log_in_console = True
+# save_log_in_file = True
+# show_log_in_console = True
