c61f781f7dd267a3481628b97941f224ae360bbf
Alexey Kurochkin <KurochkinAlexei@yandex.ru>
2025-09-03 12:25:20 +0200

    refactoring

--- app/experiments/How_to_find_HSV_values.py
+++
@@ -1,9 +0,0 @@
-import cv2 as cv
-import numpy as np
-red = 147
-green = 74
-blue = 249
-
-BGR_value = np.uint8([[[blue,green,red]]])
-HSV_value = cv.cvtColor(BGR_value, cv.COLOR_BGR2HSV)
-print(HSV_value)
